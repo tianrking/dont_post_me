fbd1c731e01e77cb2832474c6dec2444f438a4b8
w0x7ce <tian.r.king@gmail.com>
2022-11-24 21:10:32 +0800

    Update: greek.live

--- greeks.live/a.py
+++ greeks.live/a.py
@@ -1,5 +1,56 @@
 import requests
 
+class Trader:
+    
+    def __init__(self) -> None:
+        
+        pass
+        
+        # self.isRun = True
+        # self.maxPositiveCoinDelta = 1,
+        # self.maxNegativeCoinDelta = 1,
+        # self.maxPositiveUsdDelta = 1000,
+        # self.maxNegativeUsdDelta = 1000,
+        # self.positiveHedgeRatio = 100,
+        # self.negativeHedgeRatio = 100,
+        # self.coinTargetDelta = 0,
+        # self.usdTargetDelta = 0,
+        # self.eachOrderSize = 2,
+        # self.makerEachOrderSize = 20,
+        # self.longFuture = 'ETH-PERPETUAL',
+        # self.shortFuture = 'ETH-PERPETUAL',
+        # self.coinDeltaMode = False,
+        # self.orderType = 'taker',  
+    
+    def set_cookies(self,cookie_value) -> None:
+        
+        self.cookies = cookie_value
+        
+        
+    def set_headers(self,headers_value) -> None:
+        
+        self.headers = headers_value
+        
+    def get_edit_url(self,number = 1) -> str:
+        
+        if number == 1:
+            
+            return "https://www.greeks.live/ddh/strategy/edit"
+        
+    def set_eth_edit_url(self,eth_url) -> None:
+        
+        self.eth_edit_url = eth_url
+        
+        
+    def set_json_data(self,json_data) -> str:
+        
+        self.json_data = json_data
+        
+    def change(self) -> str:
+        
+        return requests.post(self.eth_edit_url, cookies=self.cookies, headers=self.headers, json=self.json_data)
+        
+    
 cookies = {
     'ph_phc_UlbOZqDIVLNVC2uoyTfUBMZ2gGvjUOc1fG9xHTKCbjZ_posthog': '%7B%22distinct_id%22%3A%22266545%22%2C%22%24device_id%22%3A%22184a9651f8a94e-0558faaedec802-7d5d5471-144000-184a9651f8b16fc%22%2C%22%24user_id%22%3A%22266545%22%2C%22%24initial_referrer%22%3A%22https%3A%2F%2Fgreeks.live%2F%22%2C%22%24initial_referring_domain%22%3A%22greeks.live%22%2C%22%24referrer%22%3A%22https%3A%2F%2Fgreeks.live%2F%22%2C%22%24referring_domain%22%3A%22greeks.live%22%2C%22%24sesid%22%3A%5B1669289287564%2C%22184a9651e80172c-02feb6c927e662-7d5d5471-144000-184a9651e811bf8%22%2C1669289287296%5D%2C%22%24active_feature_flags%22%3A%5B%5D%2C%22%24enabled_feature_flags%22%3A%7B%7D%7D',
     'session': 'eyJrZXkiOiJ2eGZCUnJ0aSJ9.Y39aGw.NyYcdENpsmeB3YvsyEh26WfOfGU',
@@ -31,7 +82,7 @@ json_data = {
     'curr': 'ETH',
     'subId': 266545,
     'param': {
-        'isRun': True,
+        'isRun': False,
         'maxPositiveCoinDelta': 1,
         'maxNegativeCoinDelta': 1,
         'maxPositiveUsdDelta': 1000,
@@ -49,5 +100,16 @@ json_data = {
     },
 }
 
-response = requests.post('https://www.greeks.live/ddh/strategy/edit', cookies=cookies, headers=headers, json=json_data)
+test_class = Trader()
+test_class.set_cookies(cookies)
+test_class.set_json_data(json_data)
+test_class.set_headers(headers)
+test_class.set_eth_edit_url('https://www.greeks.live/ddh/strategy/edit')
+
+## 0 1
+response = test_class.change()
+
+
+
+# response = requests.post('https://www.greeks.live/ddh/strategy/edit', cookies=cookies, headers=headers, json=json_data)
 print(response.text)
