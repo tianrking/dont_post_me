97d1b52ae7cd4b9a0a638e3ff63e24a78ab0e343
w0x7ce <tian.r.king@gmail.com>
2022-11-25 19:35:58 +0800

    Update: BS

--- greeks.live/buy_sell_price.py
+++ greeks.live/buy_sell_price.py
@@ -16,11 +16,11 @@ class Feed(object):
         self.url = url      # 这里输入websocket的url
         self.ws = None
     
-    def on_open(self) -> None:
+    def on_open(self,sub_param) -> None:
     
 
         # 开始订阅
-        self.sub_param = {"id":1007,"method":"public/subscribe","params":{"channels":["trades.ETH-PERPETUAL.100ms"]},"jsonrpc":"2.0"}
+        self.sub_param = sub_param
         self.sub_str = json.dumps(self.sub_param)
         print("Following Channels are subscribed!")
         print(CHANNELS_WS)
@@ -42,8 +42,11 @@ class Feed(object):
     
 # {"jsonrpc":"2.0","method":"subscription","params":{"channel":"trades.ETH-PERPETUAL.100ms","data":[{"trade_seq":112830104,"trade_id":"ETH-153702635","timestamp":1669360427665,"tick_direction":0,"price":1180.15,"mark_price":1180.19,"instrument_name":"ETH-PERPETUAL","index_price":1180.53,"direction":"buy","amount":30.0}]}}
 
+
+req_data  = {"id":1007,"method":"public/subscribe","params":{"channels":["trades.ETH-PERPETUAL.100ms"]},"jsonrpc":"2.0"}
+
 gg = Feed("wss://www.deribit.com/ws/api/v2")
-gg.on_open()
+gg.on_open(req_data)
 gg.on_url()
 gg.on_sub()
 while True:
